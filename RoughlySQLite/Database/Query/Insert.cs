2ab7d41e609ecd3ee339826aa93e558b78c30adb
naokirin <naoki.rin186@gmail.com>
2014-07-07 02:29:09 +0900

    Insertを定義する

--- RoughlySQLite/Database/Query/Insert.cs
+++ RoughlySQLite/Database/Query/Insert.cs
@@ -1,11 +1,51 @@
 ﻿using System;
+using Mono.Data.Sqlite;
 
 namespace RoughlySQLite
 {
-	public class Insert
+	public static class InsertExtension
 	{
-		public Insert()
+		public static void Insert<T>(this SqliteConnection conn)
 		{
+			try
+			{
+				new Insert(typeof(T)).Exec(conn);
+			}
+			catch(SqliteException e)
+			{
+				Console.WriteLine(e.Message);
+				throw e;
+			}
+		}
+	}
+
+	class Insert
+	{
+		string sql = "";
+
+		Type TableType { get; set; }
+
+		string TableName { get; set; }
+
+		public Insert(Type t)
+		{
+			TableType = t;
+			sql = GetSqlCommand(t);
+		}
+
+		public void Exec(SqliteConnection conn)
+		{
+			Console.WriteLine(sql + System.Environment.NewLine);
+			using(var cmd = conn.CreateCommand())
+			{
+				cmd.CommandText = sql;
+				cmd.ExecuteNonQuery();
+			}
+		}
+
+		string GetSqlCommand(Type t)
+		{
+			return "";
 		}
 	}
 }
